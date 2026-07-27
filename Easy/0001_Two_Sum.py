437fccbb8293d6faa3809891185bb676aad9f80f
Shivane "Shiv" Kottur <shivanekottur04@gmail.com>
2026-07-27 23:36:57 +0530

    Create 0001_Two_Sum.py

---
+++ Easy/0001_Two_Sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twosum(self,a,target):
+        for i in range(len(a)):
+            for j in range(i+1,len(a)):
+                if a[i]+a[j]==target:
+                    lst=[i,j]
+                    return lst
+s=Solution()                    
+arr=list(map(int,input()))
+target=int(input())
+print(s.twosum(arr,target))
