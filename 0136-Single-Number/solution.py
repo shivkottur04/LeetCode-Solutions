34c3f3a7687fde648dbaadda7cf2000b96bfa8ad
Shivane "Shiv" Kottur <shivanekottur04@gmail.com>
2026-08-03 20:27:12 +0530

    Implement singleNumber method to find unique number

---
+++ 0136-Single-Number/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for key,val in d.items():
+            if val==1:
+                return key
