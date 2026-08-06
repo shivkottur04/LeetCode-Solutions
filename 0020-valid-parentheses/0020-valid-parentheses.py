120f3bb01f3b10e6acd6ec85bc7c0f9305a05601
Shivane "Shiv" Kottur <shivanekottur04@gmail.com>
2026-08-06 10:34:02 +0530

    Time: 2 ms (81.33%), Space: 12.4 MB (96.55%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        for i in s:
+            if i in "([{":
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                if i==')' and stack[-1]=='(':
+                    stack.pop()
+                elif i==']' and stack[-1]=='[':
+                    stack.pop()
+                elif i=='}' and stack[-1]=='{':
+                    stack.pop()
+                else:
+                    return False
+        if not stack:
+            return True
+        else:
+            return False
+
+        
