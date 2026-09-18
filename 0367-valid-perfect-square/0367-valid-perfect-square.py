422f95739f5773177183337d6515575cc8c1705a
Shivane "Shiv" Kottur <shivanekottur04@gmail.com>
2026-09-18 23:04:42 +0530

    Time: 10 ms (13.36%), Space: 19.3 MB (17.81%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -0,0 +1,10 @@
+import math
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        i=1
+        while i*i<=num:
+            if i*i==num:
+                return True
+            else:
+                i+=1
+        return False
