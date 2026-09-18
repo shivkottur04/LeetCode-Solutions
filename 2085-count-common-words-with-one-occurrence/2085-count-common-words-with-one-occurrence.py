eb7ca8c22d2ed7316beab8ba9460967e81786a8b
Shivane "Shiv" Kottur <shivanekottur04@gmail.com>
2026-09-18 19:31:40 +0530

    Time: 48 ms (15.44%), Space: 19.7 MB (42.17%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -2,7 +2,7 @@ class Solution:
     def countWords(self, words1: list[str], words2: list[str]) -> int:
         count=0
         words=set(words1)
-        for i in words1:
+        for i in words:
             if i in words2 and words1.count(i)==1 and words2.count(i)==1:
                 count+=1
         return count
