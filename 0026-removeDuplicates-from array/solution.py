af3d34e155c3d8420e95427499579e26e66c6129
Shivane "Shiv" Kottur <shivanekottur04@gmail.com>
2026-09-18 21:29:00 +0530

    Delete 0026-removeDuplicates-from array directory

--- 0026-removeDuplicates-from array/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        s=set(nums)
-        unq=list(s)
-        unq.sort()
-        for i in range(len(unq)):
-            nums[i]=unq[i]
-        k=len(unq)
-        return k
