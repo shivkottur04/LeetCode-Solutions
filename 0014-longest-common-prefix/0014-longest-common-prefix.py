5e1a0aafad06fb5d7b4a173ee805b260c192ad3a
Shivane "Shiv" Kottur <shivanekottur04@gmail.com>
2026-09-20 23:30:02 +0530

    Time: 0 ms (100%), Space: 19.4 MB (32.71%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,15 +4,14 @@ class Solution:
         a=strs[0]
         b=strs[-1]
         i=0
-        j=0
         lst=[]
-        while i<len(a) and j<len(b):
-            if a[i]==b[j]:
+        while i<len(a) and i<len(b):
+            if a[i]==b[i]:
                 lst.append(a[i])
             else:
                 break
             i+=1
-            j+=1
+
         return "".join(lst)
         
         
