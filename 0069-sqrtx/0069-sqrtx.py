1baf4348d0238a1768d8028e695af8da24fe2ab3
Shivane "Shiv" Kottur <shivanekottur04@gmail.com>
2026-09-24 21:17:22 +0530

    Time: 1753 ms (6.44%), Space: 19.1 MB (97.89%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,17 @@
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x==0 or x==1:
+            return x
+        for i in range(2,x+1):
+            if i*i==x:
+                return i
+            if i*i>x:
+                return i-1
+        
+        
+                
+                
+            
+
+            
